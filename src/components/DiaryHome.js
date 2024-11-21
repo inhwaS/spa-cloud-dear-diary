fc5b383156e2741af43cc1e4b16b859fb385e60c
inhwa.son <patate.douce.pomme@gmail.com>
2024-11-20 21:55:36 -0800

    [build] added calculation for days

--- src/components/DiaryHome.js
+++ src/components/DiaryHome.js
@@ -17,15 +17,26 @@ export default function DiaryHome({ credentials }) {
     connected: false,
   });
 
+  const calculateDaysPassed = (inputDate) => {
+    const today = new Date();
+    const givenDate = new Date(inputDate);
+    const differenceInMillis = today - givenDate;
+    const daysPassed = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24));
+  
+    return daysPassed;
+  };
+  
   const getDiaryInfo = async () => {
     console.log(credentials.email);
     const diaryData = await fetchDiaryInfo(credentials.email);
     if (diaryData[0]) {
+      const days = calculateDaysPassed(diaryData[0].startDate);
       setDiaryInfo({
         diaryId: diaryData[0].diaryId,
         name1: diaryData[0].user1Name,
         name2: diaryData[0].user2Name,
         date: diaryData[0].startDate,
+        days: days,
         connected: diaryData[0].connected == 1 ? true : false,
       });
     };
